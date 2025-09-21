3b0bdc363b235b552560bd20979673852cfd3a18
Marco Zanibellato <mzanibellato@alpenite.com>
2025-09-21 23:38:24 +0200

    .

--- components/code-block.jsx
+++ components/code-block.jsx
@@ -1,9 +1,5 @@
-import { Code } from 'bright';
-
 export function CodeBlock({ code, lang, lineNumbers, title }) {
     return (
-        <Code lang={lang} title={title} lineNumbers={lineNumbers} theme="poimandres">
-            {code}
-        </Code>
+        ''
     );
 }
